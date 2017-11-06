415e78c23f98854efa70c469dcae46cb338866e6
xdarkxmatterx <33107935+xdarkxmatterx@users.noreply.github.com>
2017-11-06 13:00:40 +0800

    Update
    Modify code so it could function as intended

--- game/dota_addons/frostivus/scripts/vscripts/triggers.lua
+++ game/dota_addons/frostivus/scripts/vscripts/triggers.lua
@@ -3,22 +3,29 @@
 function Checkpoint_OnStartTouch( trigger )
 	local hHero = trigger.activator --this holds info on checkpoint trigger hero
 	local sCheckpointTriggerName = thisEntity:GetName() --this holds info on which checkpoint triggered
-	tblAccountRecord = {}
-	-- record checkpoint triggering player
-	if self._tPlayerIDToAccountRecord[hHero:GetPlayerID()] then
-		tblAccountRecord = self._tPlayerIDToAccountRecord[hHero:GetPlayerID()]
+	local trigger = 1 --Assume triggered when function called
+	local tCPRecord = {}--construct table 
+	print "all initial variable stored and declared"
+	-- record checkpoint triggering player based on each case
+	if tCPRecord[hHero:GetPlayerID()] == nil then
+		tCPRecord[hHero:GetPlayerID()] = sCheckpointTriggerName
+		trigger = 1
+		print "create new key using playerID and store this value"
+	else if string.match(tCPRecord[hHero:GetPlayerID()],sCheckpointTriggerName) then
+		trigger = 0 --set trigger to zero since repeated checkpoint
+		print "checkpoint already triggered, get out of function without doing anything"
 	else
-		self._tPlayerIDToAccountRecord[hHero:GetPlayerID()] = tblAccountRecord
+		tCPRecord[hHero:GetPlayerID()] = tCPRecord[hHero:GetPlayerID()] .. "," .. sCheckpointTriggerName
+		trigger = 1
+		print "call existing playerID and add this to the value"
 	end
-	if not tblAccountRecord["checkpoints"] then
-		tblAccountRecord["checkpoints"] = sCheckpointTriggerName
-	else
-		tblAccountRecord["checkpoints"] = tblAccountRecord["checkpoints"] .. "," .. sCheckpointTriggerName
+	
+	if trigger == 1 then
+		--[[call Panorama Ui Code Here
+		if sCheckpointTriggerName ~= "checkpoint00" then
+			BroadcastMessage( "Activated " .. sCheckpointTriggerName, 3 )
+			EmitGlobalSound( "DOTA_Item.Refresher.Activate" ) -- Checkpoint.Activate
+		end]]
 	end
-	--call Panorama Ui Code Here
-	--[[if sCheckpointTriggerName ~= "checkpoint00" then
-		BroadcastMessage( "Activated " .. sCheckpointTriggerName, 3 )
-		EmitGlobalSound( "DOTA_Item.Refresher.Activate" ) -- Checkpoint.Activate
-	end]]
 end
 
