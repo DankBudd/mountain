e07b323b0528990caedc6966c46e95b0082c5ae0
DankBudd <evilsword54@yahoo.com>
2017-11-06 09:27:26 -0500

    remove unused file

--- game/dota_addons/frostivus/scripts/vscripts/trigger.lua
+++
@@ -1,6 +0,0 @@
-function CP_OnStartTouch( keys )
-	local hero = keys.activator
-	local triggerName = thisEntity:GetName()
-	print("trigger: "..triggerName, "entered by hero: "..hero:GetUnitName())
-	
-end
