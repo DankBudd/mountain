f9d056ce11e48e8590869b0cbdb3fb54aa6d5dd1
xdarkxmatterx <33107935+xdarkxmatterx@users.noreply.github.com>
2017-11-06 16:40:30 +0800

    latest version

--- game/dota_addons/frostivus/scripts/vscripts/triggers.lua
+++ game/dota_addons/frostivus/scripts/vscripts/triggers.lua
@@ -1,26 +1,29 @@
 --[[ Snowboard Race checkpoints ]]
 
 function Checkpoint_OnStartTouch( trigger )
+	if not tCPRecord then
+		tCPRecord = {}--construct global table once
+	end
 	local hHero = trigger.activator --this holds info on checkpoint trigger hero
 	local sCheckpointTriggerName = thisEntity:GetName() --this holds info on which checkpoint triggered
-	local trigger = 1 --Assume triggered when function called
-	local tCPRecord = {}--construct table 
+	local triggerflag = 1 --Assume triggered when function called
 	print "all initial variable stored and declared"
+
 	-- record checkpoint triggering player based on each case
-	if tCPRecord[hHero:GetPlayerID()] == nil then
-		tCPRecord[hHero:GetPlayerID()] = sCheckpointTriggerName
-		trigger = 1
+	if tCPRecord[hHero:GetPlayerOwnerID()] == nil then
+		tCPRecord[hHero:GetPlayerOwnerID()] = sCheckpointTriggerName
+		triggerflag = 1
 		print "create new key using playerID and store this value"
-	else if string.match(tCPRecord[hHero:GetPlayerID()],sCheckpointTriggerName) then
-		trigger = 0 --set trigger to zero since repeated checkpoint
+	elseif string.match(tCPRecord[hHero:GetPlayerOwnerID()],sCheckpointTriggerName) then
+		triggerflag = 0 --set trigger to zero since repeated checkpoint
 		print "checkpoint already triggered, get out of function without doing anything"
 	else
-		tCPRecord[hHero:GetPlayerID()] = tCPRecord[hHero:GetPlayerID()] .. "," .. sCheckpointTriggerName
-		trigger = 1
+		tCPRecord[hHero:GetPlayerOwnerID()] = tCPRecord[hHero:GetPlayerOwnerID()] .. "," .. sCheckpointTriggerName
+		triggerflag = 1
 		print "call existing playerID and add this to the value"
 	end
 	
-	if trigger == 1 then
+	if triggerflag == 1 then
 		--[[call Panorama Ui Code Here
 		if sCheckpointTriggerName ~= "checkpoint00" then
 			BroadcastMessage( "Activated " .. sCheckpointTriggerName, 3 )
