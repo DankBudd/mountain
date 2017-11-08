90baf764cf092b1ff81dd778873471556ef31fb9
xdarkxmatterx <33107935+xdarkxmatterx@users.noreply.github.com>
2017-11-08 14:26:18 +0800

    updated code
    fix item not spawning

--- game/dota_addons/frostivus/scripts/vscripts/itemspawning.lua
+++ game/dota_addons/frostivus/scripts/vscripts/itemspawning.lua
@@ -2,46 +2,44 @@
 
 function AutoSpawnItems()
 	--list out all item spawning location in a table
-	GameRules.GameMode.tItemSpawnLocation = GameRules.GameMode.tItemSpawnLocation or function()
-    	local t = {}
-    	for _,model in pairs(Entities:FindAllByModel("logo_radiant_winter_large")) do
-    		t[model:entindex()] = model:GetAbsOrigin()
-    	end
-    	return t
-  	end
-   	--list out all items to be spawned in a table
-   	GameRules.GameMode.tItemList = GameRules.GameMode.tItemList or function()
-   		local itemkv = LoadKeyValues("scripts/npc/npc_abilities_override.txt")
-		local integer = 1
-		for key,value in pairs(itemkv) do
-    		if string.match(key, "item_") then
-        	--this is an item so store it into list
-        		GameRules.GameMode.tItemList[integer] = value
-   				integer = integer + 1
-   			end
+	if not _G.tItemSpawnLocation then
+		_G.tItemSpawnLocation = {}
+		for i=1,39 do
+			local ent = Entities:FindByName(nil, "itemspawnlocation_"..i)
+			_G.tItemSpawnLocation[i] = ent:GetAbsOrigin()
+			print "grabbing locations"
 		end
-		maxitems = integer --i want this maxvalue to be used outside of this function
-		return itemkv
 	end
-
+  	print "finish first table"
+  	PrintTable(_G.tItemSpawnLocation)
+   	--list out all items to be spawned in a table
+	if not _G.tItemList then
+		_G.tItemList = {}
+		local itemkv = LoadKeyValues("scripts/npc/npc_abilities_override.txt")
+		for key,value in pairs(itemkv) do
+			--this is an item so store it into list
+			if string.match(key, "item_") then
+				table.insert(_G.tItemList, key)
+			end
+		end
+	end
+	print "finish second table"
+	PrintTable(_G.tItemList)
    	--change time value to alter spawn frequency
-    local repeat_interval = 30 -- Rerun this timer every *repeat_interval* game-time seconds
-    local start_after = 30 -- Start this timer *start_after* game-time seconds later
-   
-	local ent = Entity:FindByName(nil, "random_item_spawner") 
-    ent:SetThink(function()
-  
-      --code to spawn item at all 39 locations
-      for i=1,39 do
-      	CreateItemOnPositionSync(GameRules.GameMode.tItemSpawnLocation[i], GameRules.GameMode.tItemList[RandomInt(0, maxitems)])
-      	--code for random item selector from item list
-		--RandomInt(0, maxitems) 	--if zero would it spawn nothing? could make it having a chance of not spanwning any item (feature)?
-		--[[Returns:int Get a random ''int'' within a range  ]]
-      end 
-
-      return repeat_interval
-    end,"timer_60",start_after)
-    
+    local repeat_interval = 15 -- Rerun this timer every *repeat_interval* game-time seconds
+    local start_after = 1 -- Start this timer *start_after* game-time seconds later
+	    
+	Timers(start_after, function()
+        --code to spawn item at all locations specified from table
+        for i=1,#_G.tItemSpawnLocation do
+            local rand = RandomInt(0, #_G.tItemList)
+        	if rand ~= 0 then
+                local item = CreateItem(_G.tItemList[rand], nil, nil)
+                CreateItemOnPositionSync(_G.tItemSpawnLocation[i], item)
+                print ("item spawned: "..item:GetName())
+           	end
+        end 
+        return repeat_interval
+    end)
 end
-
-AutoSpawnItems()
+--AutoSpawnItems()
